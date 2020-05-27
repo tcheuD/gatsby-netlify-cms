e0e379aec9e0411b9eb472275074a8e2fd4876a8
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-05-27 11:08:55 +0200

    added section to Applications

--- src/components/Home/Applications.js
+++ src/components/Home/Applications.js
@@ -6,7 +6,7 @@ import { useIntl } from 'gatsby-plugin-intl'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 
-import { PinkPaper, Image } from '../../elements'
+import { PinkPaper, Image, Section } from '../../elements'
 
 const Applications = () => {
   const intl = useIntl()
