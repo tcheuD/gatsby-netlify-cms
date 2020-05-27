bfb9be1fe214d406de424330fe9816ba4330c226
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-05-27 11:02:02 +0200

    First version page Demo (TODO white color and spacing)

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Layout, SEO } from '../components'
-import { PinkPaper } from '../elements'
+import { PinkPaper, Video } from '../elements'
 import { Grid, Typography } from '@material-ui/core'
 import DemoForm from '../components/Demo/DemoForm'
 
@@ -12,6 +12,14 @@ const DemoPage = () => {
       <Grid container spacing={3} alignItems="center">
         <Grid item xs={12} md={6}>
           <Typography variant="h2">Avantages</Typography>
+          <Video
+            videoSrcURL="https://www.youtube.com/embed/pna_yMtLrVY"
+            videoTitle="Discover TestWe"
+          />
+          <Typography variant="body1">
+            Vous êtes un étudiant et vous avez une question ?  Appelez-nous au
+            +33 (0)1 83 62 09 28 ou envoyez-nous un email à support@testwe.eu
+          </Typography>
         </Grid>
         <Grid item xs={12} md={6}>
           <PinkPaper>
