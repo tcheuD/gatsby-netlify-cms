aa97b6930bb5b98157e96ed74d1f6fdb9d52c9a0
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-05-27 10:51:41 +0200

    added favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 import Container from '@material-ui/core/Container'
+import favicon from '../images/Logos/favicon.png'
+import Helmet from 'react-helmet'
 
 import { Header, Footer } from '.'
 // import './layout.css'
@@ -19,9 +21,12 @@ export default function Layout(props) {
 
   return (
     <div>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <Container maxWidth="lg">
-        <main>{props.children}</main>
+        <main style={{ margin: '70px 0px 80px' }}>{props.children}</main>
       </Container>
       <Footer />
     </div>
