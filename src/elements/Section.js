ff799c43f686868a5444ea05336bd2a31116e938
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-06-01 11:47:04 +0200

    Mobile Section element fixed

--- src/elements/Section.js
+++ src/elements/Section.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Container, useTheme } from '@material-ui/core'
+import theme from '../gatsby-theme-material-ui-top-layout/theme'
 
 const SectionStyle = styled.section`
   height: 100vh;
   background-color: ${props => props.backgroundColor};
+  ${theme.breakpoints.down('md')} {
+    height: auto;
+  }
 `
 
 const Section = ({ children, backgroundColor, id = '' }) => {
@@ -17,7 +21,7 @@ const Section = ({ children, backgroundColor, id = '' }) => {
         backgroundColor ? backgroundColor : theme.palette.primary.main
       }
     >
-      <Container>{children}</Container>
+      {children}
     </SectionStyle>
   )
 }
